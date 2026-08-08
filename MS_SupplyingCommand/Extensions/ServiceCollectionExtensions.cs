189cf781ffe7845416517dc6323fcf6b35d7e8c8
Alex Lizano <alexsanty10lm@gmail.com>
2026-08-07 21:55:30 -0500

    feat(supplying): microservicio de presentación MS_SupplyingCommand y controladores

---
+++ MS_SupplyingCommand/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+using MS_SupplyingCommand.Middlewares;
+
+namespace MS_SupplyingCommand.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void UseErrorHandlingMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+    }
+}
