d3a01b5353f6c343b18d6ad374eebb77305a815d
Alex Lizano <alexsanty10lm@gmail.com>
2026-08-07 21:45:16 -0500

    feat(opening): DTOs para operaciones de apertura de cajas y equipos de trabajo

---
+++ OpeningApplication/DTOs/Teams/TeamByBoxResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OpeningApplication.DTOs.Teams
+{
+    public class TeamByBoxResponseDto
+    {
+            public int IdTeam { get; set; }
+            public int IdBox { get; set; }
+            public string BoxCode { get; set; }
+            public IEnumerable<TeamMemberDto> Members { get; set; }
+    }
+}
---
+++ OpeningApplication/DTOs/Teams/TeamMemberDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OpeningApplication.DTOs.Teams
+{
+    public class TeamMemberDto
+    {
+        public int UserId { get; set; }
+        public string AssignedColor { get; set; }
+    }
+}
