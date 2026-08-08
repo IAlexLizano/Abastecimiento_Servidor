e32d047aa4004bf80e2af490c8ef5c763a80229a
Alex Lizano <alexsanty10lm@gmail.com>
2026-08-07 21:45:10 -0500

    feat(opening): proyecto de aplicación, interfaces de repositorios y mapeos

---
+++ OpeningApplication/Interfaces/IOpeningRepository.cs
@@ -0,0 +1,14 @@
+﻿using OpeningApplication.DTOs.Opening;
+
+namespace OpeningApplication.Interfaces
+{
+    public interface IOpeningRepository
+    {
+        /// <summary>
+        /// Obtiene información básica (id y código) de lotes, contenedores, pallets y cajas 
+        /// que están en estado "EN_PROCESO"
+        /// </summary>
+        /// <returns>DTO con listas de información básica de lotes, contenedores, pallets y cajas</returns>
+        Task<IEnumerable<BasicInformationResponseDto>> GetBasicInformationInProcessAsync();
+    }
+}
---
+++ OpeningApplication/Mappings/GeneralMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OpeningApplication.DTOs.Opening;
+using OpeningApplication.DTOs.Teams;
+using OpeningApplication.Features.Commands;
+
+namespace OpeningApplication.Mappings
+{
+   public class GeneralMapping : Profile
+    {
+        public GeneralMapping() {
+            CreateMap<AssignTeamToBoxCommand, AssignTeamToBoxRequestDto>();
+            CreateMap<RegisterUnboxingCommand, RegisterUnboxingRequestDto>();
+            CreateMap<RegisterBoxOpeningCommand, RegisterBoxOpeningRequestDto>();
+        }
+    }
+}
